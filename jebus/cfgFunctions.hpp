6afd86dc1ab04cdaf426755bbc5cd22df8afb4c9
DreadPirateAU <38996936+DreadPirateAU@users.noreply.github.com>
2020-04-16 14:21:14 +1000

    Update cfgFunctions.hpp

--- jebus/cfgFunctions.hpp
+++ jebus/cfgFunctions.hpp
@@ -4,13 +4,24 @@ class jebus
 	{
 		file = "jebus";
 		class main {};
+		class deleteGroup {};
 		class enemyInRadius {};
 		class playerInRadius {};
 		class pilotKill {};
 		class reduce {};
 		class cache {};
+		class recruit {};
+		class saveGroup {};
+		class saveUnit {};
+		class saveVehicle {};
+		class spawnGroup {};
+		class spawnUnit {};
+		class spawnVehicle {};
 		class saveWaypoints {};
 		class applyWaypoints {};
+		class unstick {};
+		class dismiss {};
+		class new {};
 	};
 
 };
